ce6a384204c4d63c3b6fe72f313f2596945da20c
nicogulo <nicogulobelas@gmail.com>
2021-08-09 14:36:33 +0700

    0.4.1

--- src/pages/createPlaylist/index.js
+++ src/pages/createPlaylist/index.js
@@ -3,12 +3,25 @@ import React, { useState, useEffect } from 'react';
 import TrackList from '../../components/trackList';
 import MessageNotFound from '../../components/messageNotFound';
 import Button from '../../components/button';
+import FormCreateNewPlaylist from '../../components/formCreateNewPlaylist';
+
+// ! redux area
+import { useSelector, useDispatch } from 'react-redux';
+import { setToken } from '../../redux/tokenSlice';
+
 import style from './createPlaylist.module.css';
 
-import responsetGetSearchTrack from '../../functions/responsetGetSearchTrack';
+import {
+  getProfile,
+  getSearchTracks,
+  createNewPlaylist,
+  storeTracksToNewPlaylist,
+} from '../../services/apiSpotify';
 
-export default function CreatePlaylist({ getAccessTokenFromURL, getProfile }) {
-  const [token, setToken] = useState('');
+export default function CreatePlaylist({ getAccessTokenFromURL }) {
+  const token = useSelector((state) => state.token.value);
+  const dispatch = useDispatch();
+
   const [userID, setUserID] = useState('');
   const [search, setSearch] = useState('');
   const [tracks, setTracks] = useState([]);
@@ -20,78 +33,43 @@ export default function CreatePlaylist({ getAccessTokenFromURL, getProfile }) {
     collaborative: false,
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setPostPlaylist({ ...postPlaylist, [name]: value });
-  };
-
   useEffect(() => {
     if (window.location.hash) {
       const { access_token } = getAccessTokenFromURL(window.location.hash);
-      setToken(access_token);
+
+      dispatch(setToken(access_token));
+
       getProfile(access_token).then((data) => setUserID(data.id));
     }
-  }, [getAccessTokenFromURL]);
+  }, [dispatch, getAccessTokenFromURL]);
 
   const buttonHandleSearch = () => {
     if (search === '') {
-      alert('Search Cannot Empty');
+      alert('Search Cannot Be Empty');
     } else {
-      responsetGetSearchTrack(search, token, setTracks);
+      getSearchTracks(search, token).then((data) =>
+        setTracks(data.tracks.items)
+      );
     }
-  };
-
-  const createNewPlaylist = (obj) => {
-    fetch(`https://api.spotify.com/v1/users/${userID}/playlists`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + token,
-      },
-      body: JSON.stringify({
-        name: obj.name,
-        public: false,
-        collaborative: false,
-        description: obj.description,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => storeTrackstoPlaylist(data.id));
-  };
-
-  const storeTrackstoPlaylist = (playlistId) => {
-    const uri = playlist.map((track) => track);
-
-    fetch(
-      `https://api.spotify.com/v1/playlists/${playlistId}/tracks?position=0&uris=${uri}`,
-      {
-        method: 'POST',
-        headers: {
-          Authorization: 'Bearer ' + token,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          uris: uri,
-          position: 0,
-        }),
-      }
-    )
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-
-    setPlaylist([]);
-    setPostPlaylist({
-      name: '',
-      description: '',
-      public: false,
-      collaborative: false,
-    });
-    alert('Create Playlist berhasil');
   };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    createNewPlaylist(postPlaylist);
+    if (playlist.length <= 0) {
+      alert('You have to selected song first');
+    } else {
+      createNewPlaylist(userID, token, postPlaylist).then((newPlaylist) =>
+        storeTracksToNewPlaylist(newPlaylist.id, token, playlist).then((data) =>
+          console.log(data)
+        )
+      );
+      alert('Create New Playlist Has Been Successfully');
+      setPostPlaylist({
+        name: '',
+        description: '',
+      });
+      setPlaylist([]);
+    }
   };
 
   return (
@@ -113,39 +91,24 @@ export default function CreatePlaylist({ getAccessTokenFromURL, getProfile }) {
         </Button>
       </div>
 
-      <div className={style['list-track']}>
-        {tracks.length > 0 ? (
+      {tracks.length > 0 ? (
+        <div className={style['list-track']}>
+          <FormCreateNewPlaylist
+            postPlaylist={postPlaylist}
+            setPostPlaylist={setPostPlaylist}
+            handleFormSubmit={handleFormSubmit}
+          />
           <TrackList
             tracks={tracks}
             playlist={playlist}
             setPlaylist={setPlaylist}
           />
-        ) : (
-          <MessageNotFound search={search} />
-        )}
-      </div>
+        </div>
+      ) : (
+        <MessageNotFound search={search} />
+      )}
 
-      <div className={style['form']}>
-        <form onSubmit={handleFormSubmit}>
-          <label>Name Playlist</label>
-          <input
-            id="name"
-            name="name"
-            type="text"
-            minLength="10"
-            value={postPlaylist.name}
-            onChange={handleChange}
-          />
-          <label>Description</label>
-          <textarea
-            id="description"
-            name="description"
-            minLength="20"
-            onChange={handleChange}
-          ></textarea>
-          <Button type="submit">Create Playlist</Button>
-        </form>
-      </div>
+      {/* <div className={style["form-create-playlist"]}></div> */}
     </div>
   );
 }
