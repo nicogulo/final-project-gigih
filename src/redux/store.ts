6c0c338292f09edb84ee977e101deccdd7d51063
nicogulo <nicogulobelas@gmail.com>
2021-08-15 12:48:07 +0700

    change ext to tsx and fix

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,15 +3,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import playlistReducer from './playlistSlice';
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-
 const store = configureStore({
   reducer: {
     auth: authReducer,
     playlist: playlistReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
